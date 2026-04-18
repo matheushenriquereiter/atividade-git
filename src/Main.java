0ba1f96d4d19b5c625fa9fe4797140509116c212
danielgarciadasilva <daniel.garciadasilva3@gmail.com>
2026-04-17 21:45:25 -0300

    Adiciona for exibindo Jorge

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Jorge");
+        }
     }
 }
 
